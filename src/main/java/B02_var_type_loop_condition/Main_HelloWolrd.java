20d2e45d9be6014564ae2cc9d8181f6ab8a3ab42
SuharuYuu <longlh2727@gmail.com>
2021-10-22 16:31:26 +0700

    Initial commit

---
+++ src/main/java/B02_var_type_loop_condition/Main_HelloWolrd.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package B02_var_type_loop_condition;
+
+/**
+ *
+ * @author MSI - MEDIAMART
+ */
+public class Main_HelloWolrd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
