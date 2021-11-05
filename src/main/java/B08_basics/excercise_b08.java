f504a2e14c0d7a6f9aab20ce694ffb8336ba33a3
SuharuYuu <longlh2727@gmail.com>
2021-11-05 15:42:29 +0700

    bai 13 B08

---
+++ src/main/java/B08_basics/excercise_b08.java
@@ -0,0 +1,45 @@
+package B08_basics;
+
+import java.util.Scanner;
+
+public class excercise_b08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner inPut = new Scanner(System.in);
+
+        System.out.println("Input max array: ");
+        int N = inPut.nextInt();
+        int[] phanTu = new int[N];
+        for(int i = 0; i < N; i++){
+            phanTu[i] = inPut.nextInt();
+        }
+
+        int Max = phanTu[0];
+        System.out.println("Max number: ");
+        MaxNum(Max, N, phanTu);
+
+        int Min = phanTu[0];
+        System.out.println("Min number: ");
+        MinNum(Min, N, phanTu);
+    }
+    public static void MaxNum(int Max, int N, int[] arr) {
+        for(int i = 0; i < N; i++){
+            if(Max < arr[i]){
+                Max = arr[i];
+            }
+        }
+        System.out.println("Max: "+Max);
+    }
+    public static void MinNum(int Min, int N, int[] arr){
+        for(int i = 0; i< N; i++){
+            if(Min > arr[i]){
+                Min = arr[i];
+            }
+        }
+        System.out.println("Min: "+Min  );
+    }
+
+}
